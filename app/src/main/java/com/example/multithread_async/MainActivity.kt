21b6e89900e9bfd8f18ade294eda7e450df5dc63
Stanislav_Soviak <stas.soviak@gmail.com>
2023-10-12 15:43:44 +0300

    rxJava implementation

--- app/src/main/java/com/example/multithread_async/MainActivity.kt
+++ app/src/main/java/com/example/multithread_async/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.multithread_async
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
 import com.example.multithread_async.databinding.ActivityMainBinding
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.disposables.CompositeDisposable
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,10 +17,25 @@ class MainActivity : AppCompatActivity() {
         MainAdapter()
     }
 
+    private val compositeDisposable = CompositeDisposable()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         binding.list.adapter = listAdapter
+
+        val numbersDisposable = viewModel.numbersObservable
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe { newList ->
+                listAdapter.submitList(newList)
+            }
+
+        compositeDisposable.add(numbersDisposable)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        compositeDisposable.clear()
     }
 }
--- app/src/main/java/com/example/multithread_async/MainViewModel.kt
+++ app/src/main/java/com/example/multithread_async/MainViewModel.kt
@@ -1,15 +1,43 @@
 package com.example.multithread_async
 
 import androidx.lifecycle.ViewModel
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.disposables.CompositeDisposable
+import io.reactivex.rxjava3.schedulers.Schedulers
+import io.reactivex.rxjava3.subjects.BehaviorSubject
+import java.util.concurrent.TimeUnit
 import kotlin.random.Random
 
 class MainViewModel : ViewModel() {
+
+    private val compositeDisposable = CompositeDisposable()
+    private val numbersSubject = BehaviorSubject.createDefault(emptyList<Int>())
+
+    val numbersObservable: Observable<List<Int>> = numbersSubject.hide()
+
 
     init {
         getRandomNumber()
     }
 
-    private fun getRandomNumber(){
-        val number = Random.nextInt(100)
+    private fun getRandomNumber() {
+        val randomNumberObservable = Observable.interval(2, TimeUnit.SECONDS)
+            .map { Random.nextInt(100) }
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+
+        val disposable = randomNumberObservable.subscribe { number ->
+            val currentList = numbersSubject.value.orEmpty().toMutableList()
+            currentList.add(number)
+            numbersSubject.onNext(currentList)
+        }
+
+        compositeDisposable.add(disposable)
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        compositeDisposable.clear()
     }
 }
